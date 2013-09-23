2ca84540b8296b34da71105762d49025824118cc
foobarfoobar <chndchnd@gmail.com>
2013-09-23 15:33:02 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,19 @@
 ## new file
+
+class Good(Object):
+	pass
+
+class FactoryProto(Object):
+	pass
+
+class PopProto(Object):
+	pass
+
+class Factory(Object):
+    pass
+
+class Pop(Object):
+	pass
+
+class WorldMarket(Object):
+	pass
